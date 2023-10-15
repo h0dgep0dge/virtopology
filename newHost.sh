21f761644ac0d3d2cb680bd339473290766a971b
Robbie Mckennie <h0dgep0dge>
2023-10-15 22:07:10 +1300

    Added a flag to newHost to enable ip forwarding

--- newHost.sh
+++ newHost.sh
@@ -2,9 +2,10 @@
 
 usage() {
     cat << EOF
-Usage: $0 NAME [-h | --help]
+Usage: $0 NAME [-f] [-h | --help]
 
     NAME    The name of the host to create
+    -f      Enable IP forwarding on this host
     --help  Show this message
 EOF
 exit 1
@@ -16,10 +17,12 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 pos_arguments=()
+forwarding=""
 
 while [[ -n $1 ]] ; do
     case $1 in
-        -h | --help) usage;;
+        -h | --help) usage ;;
+        -f) forwarding="YES"; shift ;;
         *) pos_arguments+=("$1") ; shift ;;
     esac
 done
@@ -30,5 +33,10 @@ if [[ -z $name ]]; then
     usage
 fi
 
-ip netns add $name
+ip netns add "$name"
 ip -n "$name" link set lo up
+
+if [[ -n $forwarding ]]; then
+    ip netns exec "$name" sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+    ip netns exec "$name" sh -c "echo 1 > /proc/sys/net/ipv6/conf/all/forwarding"
+fi
